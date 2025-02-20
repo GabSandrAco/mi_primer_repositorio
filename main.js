d3ec45a5cb77ab9266d0fb69ab27a7906a80cb17
Carlos Alberto Martinez Barrera <mabc920819@hotmail.com>
2025-02-20 00:23:49 -0700

    Agregado pruebas de javascript con objetos y arreglos + css

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("Hola mundo desde javascript")
+console.log("Hola mundo desde javascript");
+
+let persona =  {
+    "nombre":"carlos",
+    "edad":32,
+    "domicilio":{
+        "calle":"Londres",
+        "colonia":"Lomas de san isidro",
+        "codigo":
+            ["Carlos","sandra","toyota"]
+    
+    }
+}
+
+let miArreglo = ["Mazda", "Toyota", "Suzuki"]
+
+
+console.log(miArreglo.length);
+
+miArreglo.forEach(elemento => {
+    console.log(elemento)
+});
